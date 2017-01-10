223bd4eb8ab7b93428baac5c9427a97a7697d3b8
Chris Lim-Davies <chris.lim-davies@au.tribalworldwide.com>
2017-01-10 16:49:51 +1100

    Modified auto build shell commands.

--- automated_build.sh
+++ automated_build.sh
@@ -1,20 +1,5 @@
-# Copy static site
-CWD=`pwd`
-
-# Clone Pages repository
-cd /tmp
-git clone https://github.com/tribal-chrisdavies/tribal-chrisdavies.github.io.git build
-# cd build && git checkout -b master origin/master # If not using master
-
-# Trigger Jekyll rebuild
-cd $CWD
 bundle exec jekyll contentful
 bundle exec jekyll build
-
-# Push newly built repository
-cp -r $CWD/_build/* /tmp/build # or $CWD/_site
-
-cd /tmp/build
 
 git config --global user.email "chris.lim-davies@au.tribalworldwide.com"
 git config --global user.name "Chris Davies"
