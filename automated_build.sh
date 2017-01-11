e3ca3d0364ffc260c8c88f0499091faf7d70efee
Chris Lim-Davies <chris.lim-davies@au.tribalworldwide.com>
2017-01-11 12:36:52 +1100

    Modified git handling to push to master.

--- automated_build.sh
+++ automated_build.sh
@@ -1,13 +1,10 @@
 git config --global user.email "chris.lim-davies@au.tribalworldwide.com"
 git config --global user.name "Chris Davies (CircleCI)"
 
-git checkout gh-pages 2>/dev/null || git checkout -b gh-pages
-git merge master
-
 bundle exec jekyll contentful --rebuild --trace
 git add .
 
 CURDATE=$(TZ=":Australia/Sydney" date +'%r (%d-%m-%Y, %Z)')
 git commit -m "Automated rebuild at $CURDATE."
 
-git push -f origin gh-pages
+git push -f origin master
