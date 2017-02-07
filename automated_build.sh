6713108fb134ac3c6536cea66f0172ee00228507
Chris Lim-Davies <chris.lim-davies@au.tribalworldwide.com>
2017-02-07 16:13:09 +1100

    Modified automated build script to do more stuff.

--- automated_build.sh
+++ automated_build.sh
@@ -1,10 +1,24 @@
+# Set up git
 git config --global user.email "Technical@ddb.com.au"
 git config --global user.name "CircleCI AutoBuilder"
 
-ruby data_importer.rb
-git add .
+# Change to MASTER branch with a clean copy
+git checkout -f
+git checkout master
+git fetch origin master
+git reset --hard origin/master
 
+# Update data
+ruby data_importer.rb
+
+# Get _site/* files and push them to MASTER branch
+# Note: CircleCI creates vendor and .bundle files
+mv _site /tmp/
+rm -rf * .bundle .sass-cache
+mv /tmp/_site/* .
+
+# Commit changes
+git add -A .
 CURDATE=$(TZ=":Australia/Sydney" date +'%r (%d-%m-%Y, %Z)')
 git commit -m "Automated CircleCI rebuild at $CURDATE."
-
 git push -f origin master
